e4f1eab57150257c72461180aa536a2b926ea9cb
plam <plam@localhost.dev>
2020-10-08 13:54:53 +0000

    05_05 Demo: Simple Routing

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -4,6 +4,7 @@ const HomePage = () => (
     <div className="jumbotron">
         <h1>Pluralsight Administration</h1>
         <p>React, Flux, and React Router for ultra-responsive web apps.</p>
+        <a href="/about">About Page</a>
     </div>
 );
 
--- src/index.js
+++ src/index.js
@@ -1,11 +1,11 @@
 import React from "react";
 import { render } from "react-dom";
-import HomePage from './components/HomePage';
-import AboutPage from './components/AboutPage';
+
+import App from './components/App';
 
 import "bootstrap/dist/css/bootstrap.min.css";
 
 render(
-    <AboutPage />,
+    <App />,
     document.getElementById('root')
 );
