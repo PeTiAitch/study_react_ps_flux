7dab5bc9a69825cba080a30ad208cbfaaf3848c6
plam <plam@localhost.dev>
2020-10-08 11:36:53 +0000

    05_03 Demo: Create App Entry Point

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -1,7 +1,7 @@
 import React from 'react';
 
 const HomePage = () => (
-    <div classname="jumbotron">
+    <div className="jumbotron">
         <h1>Pluralsight Administration</h1>
         <p>React, Flux, and React Router for ultra-responsive web apps.</p>
     </div>
--- src/index.js
+++ src/index.js
@@ -1,1 +1,10 @@
+import React from "react";
+import ReactDOM, { render } from "react-dom";
+import HomePage from './components/HomePage';
+
 import "bootstrap/dist/css/bootstrap.min.css";
+
+render(
+    <HomePage />,
+    document.getElementById('root')
+);
