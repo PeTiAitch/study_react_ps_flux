cc617c060125b829cc9c35ae53d053272f262ed2
plam <plam@localhost.dev>
2020-10-13 15:14:44 +0000

    06_07 Summary - Props, State, Lifecycle Methods, and Keys

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,7 @@
 import React from "react";
 import HomePage from "./HomePage";
 import AboutPage from "./AboutPage";
+import CoursesPage from './CoursesPage';
 import Header from "./common/Header";
 
 const App = () => {
@@ -9,6 +10,10 @@ const App = () => {
 
     if (route === "/about") {
       return <AboutPage />;
+    }
+
+    if (route === "/courses") {
+      return <CoursesPage />;
     }
 
     return <HomePage />;
