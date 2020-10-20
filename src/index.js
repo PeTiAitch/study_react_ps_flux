eeab5c3efc50960b39846e1e7fa03bfcba37c623
plam <plam@localhost.dev>
2020-10-20 10:49:56 +0000

    08_03 Demo: Initial Configuration

--- src/index.js
+++ src/index.js
@@ -1,11 +1,14 @@
 import React from "react";
 import { render } from "react-dom";
+import { BrowserRouter as Router } from "react-router-dom";
 
 import App from './components/App';
 
 import "bootstrap/dist/css/bootstrap.min.css";
 
 render(
-    <App />,
-    document.getElementById('root')
+  <Router>
+    <App />
+  </Router>,
+  document.getElementById("root")
 );
