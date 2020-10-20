2a3a83d538d86f38448ecd16536ef0a10caadf67
plam <plam@localhost.dev>
2020-10-20 11:23:30 +0000

    08_06 Demo: Links

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -1,10 +1,11 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const HomePage = () => (
     <div className="jumbotron">
         <h1>Pluralsight Administration</h1>
         <p>React, Flux, and React Router for ultra-responsive web apps.</p>
-        <a href="/about">About Page</a>
+        <Link to="about" className="btn btn-primary">About Page</Link>
     </div>
 );
 
