b71edacd8ef7e0d83e034ae35c6e756f23fa9618
plam <plam@localhost.dev>
2020-10-21 11:46:06 +0000

    08_07 Demo: NavLinks

--- src/components/common/Header.js
+++ src/components/common/Header.js
@@ -1,10 +1,26 @@
-import React from 'react';
+import React from "react";
+import { NavLink } from "react-router-dom";
 
-const Header = () => (
-  <nav>
-    <a href="/">Home</a> | <a href="/courses">Courses</a> |{" "}
-    <a href="/about">About</a>
-  </nav>
-);
+const Header = () => {
+  const activeStyle = {
+    color: "orange",
+  };
+
+  return (
+    <nav>
+      <NavLink to="/" exact activeStyle={activeStyle}>
+        Home
+      </NavLink>
+      {" | "}
+      <NavLink to="/courses" activeStyle={activeStyle}>
+        Courses
+      </NavLink>
+      {" | "}
+      <NavLink to="/about" activeStyle={activeStyle}>
+        About
+      </NavLink>
+    </nav>
+  );
+};
 
 export default Header;
