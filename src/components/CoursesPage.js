a5a5ff557b4c986341f3dafe158fe80c266df4e5
plam <plam@localhost.dev>
2021-01-25 21:03:26 +0000

    09_01 Create Add Course Button

--- src/components/CoursesPage.js
+++ src/components/CoursesPage.js
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from "react";
+import { Link } from 'react-router-dom';
 import { getCourses } from "../api/courseApi";
 import CourseList from './CourseList';
 
@@ -14,6 +15,7 @@ function CoursesPage() {
   return (
     <>
       <h2>Courses</h2>
+      <Link to="/course" className="btn btn-primary">Add Course</Link>
       <CourseList courses={courses} />
     </>
   );
--- src/components/ManageCoursePage.js
+++ src/components/ManageCoursePage.js
@@ -5,7 +5,7 @@ const ManageCoursePage = (props) => {
     return (
         <>
             <h2>Manage Course</h2>
-            <Prompt when={true} message="Are you sure?" />
+            {/* <Prompt when={true} message="Are you sure?" /> */}
             {props.match.params.slug}
         </>
     );
