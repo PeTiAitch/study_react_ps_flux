ce107f7f256955660e3c6eba3434e886312a420e
plam <plam@localhost.dev>
2021-01-26 18:04:10 +0000

    09_04 Form State and Change Handlers

--- src/components/ManageCoursePage.js
+++ src/components/ManageCoursePage.js
@@ -1,12 +1,26 @@
-import React from 'react';
+import React, { useState } from 'react';
+
 
 import CourseForm from './CourseForm';
 
 const ManageCoursePage = (props) => {
+    const [course, setCourse] = useState({
+        id: null,
+        slug: '',
+        title: '',
+        authorId: null,
+        category: ''
+    });
+
+    const handleTitleChange = (e) => {
+        const updatedCourse = {...course, title: e.target.value};
+        setCourse(updatedCourse);
+    }
+
     return (
         <>
             <h2>Manage Course</h2>
-            <CourseForm />
+            <CourseForm course={course} onTitleChange={handleTitleChange} />
             {props.match.params.slug}
         </>
     );
