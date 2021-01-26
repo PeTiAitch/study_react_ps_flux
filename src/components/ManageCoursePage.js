f52166577566ed09bfb1906088818bb8bda7ce0d
plam <plam@localhost.dev>
2021-01-26 12:57:11 +0000

    09_02 Create Course Form

--- src/components/ManageCoursePage.js
+++ src/components/ManageCoursePage.js
@@ -1,11 +1,12 @@
 import React from 'react';
-import { Prompt } from "react-router-dom";
+
+import CourseForm from './CourseForm';
 
 const ManageCoursePage = (props) => {
     return (
         <>
             <h2>Manage Course</h2>
-            {/* <Prompt when={true} message="Are you sure?" /> */}
+            <CourseForm />
             {props.match.params.slug}
         </>
     );
